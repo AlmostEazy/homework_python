15aa6bfb87604f63461cb4ee98c44109299568cd
AlmostEazy <Antosha101992@rambler.ru>
2022-11-29 18:12:55 +0300

    feat: fix task_3.py file

--- task_3_1/task_3.py
+++ task_3_1/task_3.py
@@ -2,6 +2,7 @@
  Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369."""
 
 first_n = input("Введите число n: ")
+
 second_n = first_n + first_n
 third_n = first_n + first_n + first_n
 count = int(first_n) + int(second_n) + int(third_n)
