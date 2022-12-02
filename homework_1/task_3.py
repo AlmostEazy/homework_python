d93303a3be15f70b5249ffde3306ce7e1ff5c875
AlmostEazy <Antosha101992@rambler.ru>
2022-12-02 20:23:03 +0300

    feat: add homework_1 files

---
+++ homework_1/task_3.py
@@ -0,0 +1,11 @@
+"""3) Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
+ Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369."""
+
+first_n = input("Введите число n: ")
+
+second_n = first_n + first_n
+third_n = first_n + first_n + first_n
+count = int(first_n) + int(second_n) + int(third_n)
+
+print(f"Ваши значения: {first_n}, {second_n}, {third_n}")
+print(f"Значение выражения n + nn + nnn = {count}")
